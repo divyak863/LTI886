8728165bbd38704e79d2baa61d56c1dee9966218
Gayathri Mahadevan <gayat_19@yahoo.com>
2020-11-23 17:38:58 +0530

    Day2_Codility

---
+++ Day1_Codility/CodilityDay1Solution/CodilityDay1Project/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CodilityDay1Project
+{
+    class Program
+    {
+
+        bool CreditCardCheck(string number)
+        {
+            int N = 32;
+            //string binaryNumbers = Convert.ToString(N, 2);
+            ////Console.WriteLine(binaryNumbers);
+            //int longGap = 0, currentGap = 0;
+            //int idx = binaryNumbers.LastIndexOf('1');
+            //if (idx < binaryNumbers.Length - 1)
+            //    binaryNumbers = binaryNumbers.Substring(0, idx);
+            //Console.WriteLine(binaryNumbers);
+
+            //string binaryNumbers = Convert.ToString(N, 2);
+            //Regex requiredPattern = new Regex("(?<=1)(0*)(?=1)");
+            //var gap = requiredPattern.Matches(binaryNumbers)
+            //            .Cast<Match>()
+            //            .Select(str => str.Length)
+            //            .Max();
+            //Console.WriteLine(gap);
+
+            int i = 6;
+            int j = i ^ 6;
+            j = j ^ 2;
+            j = j ^ 5;
+            j = j ^ 4;
+            j = j ^ 2;
+            j = j ^ 5;
+            Console.WriteLine(j);
+            //if (number.Length != 16)
+            //    return false;
+            //List<int> numbers = new List<int>();
+            //for (int i = 0; i < 16; i++)
+            //{
+            //    numbers.Add(Convert.ToInt32(number[i].ToString()));
+            //}
+            //numbers.Reverse();
+            //int sum = 0;
+            //for (int i = 0; i < 16; i++)
+            //{
+            //    if(i%2 !=0)
+            //    {
+            //        numbers[i] *= 2;
+            //        if (numbers[i] > 9)
+            //            numbers[i] -= 9;
+            //    }
+            //    sum += numbers[i];
+            //}
+            //if (sum % 10 == 0)
+            //    return true;
+            return false;
+        }
+        void PrintRightAngleTriangle()
+        {
+            int size = 0;
+            Console.WriteLine("Please enter the number of lines");
+            while (!int.TryParse(Console.ReadLine(),out size))
+            {
+                Console.WriteLine("Invalid number");
+            }
+            for (int i = 1; i < size; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool res = new Program().CreditCardCheck("4477468343113004");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day2_Codility/Day2ColilitySolution/Day2ColilityProject/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2ColilityProject
+{
+    class Program
+    {
+        public int solution(int[] A)
+        {
+            double sum = A.Sum(e => (double)e);
+            int num1 = A[0];
+            int num2 = (int)sum - A[0];
+            int result = Math.Abs(num1 - num2);
+            for (int i = 1; i < A.Length-1; i++)
+            {
+                num1 += A[i];
+                num2 -= A[i];
+                int diff = Math.Abs(num1 - num2);
+                if (result > diff)
+                    result = diff;
+            }
+            return 0;
+        }
+
+        int[] Counting(int[] A, int m)
+        {
+            int[] result = new int[m+1];
+            for (int i = 0; i < A.Length; i++)
+            {
+                result[A[i]] += 1;
+            }
+            return result;
+
+        }
+        bool CheckSwap(int[] A, int[] B, int m)
+        {
+            int length = A.Length;
+            double sum_A = A.Sum();
+            double sum_B = B.Sum();
+            double d = sum_A - sum_B;
+            if(d%2 ==1)
+                return false;
+            d = Math.Floor(d /2);
+            int[] count = Counting(A, m);
+            for (int i = 0; i < length; i++)
+            {
+                if ((0 <= B[i] - d) && (B[i] - d < m) && (count[B[i] - (int)d] > 0))
+                    return true;
+            }
+            return false;
+        }
+
+        int FrogJump(int[] A, int X)
+        {
+            int res = A.ToList().IndexOf(X);
+            int result = -1;
+            int[] temp = new int[X];
+            int no_of_leaves = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(temp[A[i]-1]==0)
+                {
+                    temp[A[i] - 1] += 1;
+                    no_of_leaves++;
+                    if(no_of_leaves==X)
+                    {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1,4,2,7 };
+            int[] arr2 = { 3, 6, 2, 1 };
+            //int[] res = new Program().Counting(arr,5);
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            int[] A = { 1, 3,1,4,2,3,5,4 };
+            Console.WriteLine(new Program().FrogJump(A, 5));
+            Console.ReadKey();
+        }
+    }
+}
